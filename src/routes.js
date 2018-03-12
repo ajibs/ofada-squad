1ac5f035aea4cdac3fba8213bf5e46e6c3db3f28
Bolu Ajibawo <bolu.ajibawo@Administrators-MacBook-Air.local>
2018-03-12 11:07:15 +0100

    route to try out stuff

--- src/routes.js
+++ src/routes.js
@@ -4,25 +4,13 @@ import sendFoodFormToUser from './controllers/sendFoodFormToUser';
 import getFoodOrders from './controllers/getOrders';
 import showHomePage from './controllers/showHome';
 import totalPriceForToday from './controllers/totalPrice';
-
+// import testing from './controllers/testing';
 
 const router = new express.Router();
 
 router.get('/', showHomePage);
 
-/*
-router.post('/test', (req, res) => {
-  const foodItems = req.body.foodItems
-    .split('\n') // split into array by /n  e.g. [" Rice  200  ", " Dodo  100  ", " Meat  100", " Egg  50"]
-    .map((elem) => {
-      const item = elem.replace(/[^a-z]/gi, ''); // filter out everything except alphabets
-      const amount = elem.replace(/[^0-9]/g, ''); // filter out everything except numbers
-      return [item, amount];
-    });
-
-  res.json({ foodItems });
-});
-*/
+// router.post('/test', testing);
 
 // food order dialog form
 router.post('/slack/command/food', sendFoodFormToUser);
