a03866ae89671694cbb247c4956725399d567b65
Bolu Ajibawo <bolu.ajibawo@Administrators-MacBook-Air.local>
2018-03-23 13:10:04 +0100

    better message for invalid order

--- build/responses/invalidOrder.js
+++ build/responses/invalidOrder.js
@@ -8,7 +8,7 @@ function invalidOrder(slackData, userName) {
     responseUrl: slackData.response_url,
     text: `You made a mistake *${userName}*,\n
     please enter the price not quantity of an item\n
-    i.e. *egg - 50* not *egg - 1*
+    e.g. *egg - 50* not *egg - 1*
     `,
     mrkdwn: true,
     mrkdwn_in: ['text']
